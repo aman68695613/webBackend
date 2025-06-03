853fc96c1fedd58c1a28a4116c66028c6baec94d
aman68695613 <amansrivastav6869@gmail.com>
2025-06-03 16:15:06 +0530

    werrwe

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, "../frontend")));
+// app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.post("/screenshot", async (req, res) => {
   const browser = await puppeteer.launch({
